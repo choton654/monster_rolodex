f90312a172bbbfbf2a4237b4ec5106c1ad7a2cf8
Choton <choton654@gmail.com>
2020-06-15 14:38:40 +0530

    Updates

---
+++ precache-manifest.b768389a0db0f0e38146279240f1c809.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08d39ebb7bf43030b0ca58852b504a3c",
+    "url": "/monster_rolodex/index.html"
+  },
+  {
+    "revision": "3aff340ebd25fa5758ec",
+    "url": "/monster_rolodex/static/css/main.c4278223.chunk.css"
+  },
+  {
+    "revision": "6f94c0c218eb3cb14e13",
+    "url": "/monster_rolodex/static/js/2.a7d23887.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster_rolodex/static/js/2.a7d23887.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3aff340ebd25fa5758ec",
+    "url": "/monster_rolodex/static/js/main.c99892b7.chunk.js"
+  },
+  {
+    "revision": "18c6c27140aa9379fc0c",
+    "url": "/monster_rolodex/static/js/runtime-main.47721e75.js"
+  }
+]);
